c61e9d119011e8571e81605f6af9974c1f94e1d5
kongduu <158478279+kongduu@users.noreply.github.com>
2025-02-25 18:06:28 +0900

    Update MyIntro.js

--- src/components/MyIntro.js
+++ src/components/MyIntro.js
@@ -3,7 +3,7 @@ import React from "react";
 function MyIntro({ intro, message }) {
   return (
     <div>
-      <h2>내 소개</h2>
+      <h2>소개</h2>
       <p>{intro}</p>
       <p><strong>{message}</strong></p>
     </div>
