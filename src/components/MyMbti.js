da77dacde58b173d10cf66984e4ea966c670a632
kongduu <158478279+kongduu@users.noreply.github.com>
2025-02-25 18:06:38 +0900

    Update MyMbti.js

--- src/components/MyMbti.js
+++ src/components/MyMbti.js
@@ -3,7 +3,7 @@ import React from "react";
 function MyMbti({ mbti, description }) {
   return (
     <div>
-      <h2>내 MBTI</h2>
+      <h2>MBTI</h2>
       <p>제 MBTI는 <strong>{mbti}</strong> 입니다.</p>
       <p>{description}</p>
     </div>
